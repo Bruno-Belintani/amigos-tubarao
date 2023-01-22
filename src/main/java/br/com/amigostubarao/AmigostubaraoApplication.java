490026d3a10013e4d45b629e3f54e678c88c8602
kekeison <kekeisonmaster@gmail.com>
2023-01-21 22:29:30 -0300

    inicio da estrutura do projeto

---
+++ src/main/java/br/com/amigostubarao/AmigostubaraoApplication.java
@@ -0,0 +1,13 @@
+package br.com.amigostubarao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigostubaraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigostubaraoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amigostubarao/AmigostubaraoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amigostubarao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigostubaraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
