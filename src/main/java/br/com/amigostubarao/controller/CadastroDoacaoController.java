4d26e42c2ea970deed039877c2e226108dc6de41
Claudinei.Porto <claudineiporton@gmail.com>
2023-02-10 21:06:33 -0300

    teste Cau

--- src/main/java/br/com/amigostubarao/controller/CadastroDoacaoController.java
+++ src/main/java/br/com/amigostubarao/controller/CadastroDoacaoController.java
@@ -1,6 +1,7 @@
 package br.com.amigostubarao.controller;
-
+import br.com.amigostubarao.model.CadastroDoacao;
 import br.com.amigostubarao.model.DadosPessoais;
+import br.com.amigostubarao.model.InformacoesAdicionais;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,17 +9,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/dados-pessoais")
+@RequestMapping ("/cadastro-doacao")
 
-public class DadosPessoaisController {
+public class CadastroDoacaoController {
 
-    Logger logger = LoggerFactory.getLogger(DadosPessoaisController.class);
+    Logger logger = LoggerFactory.getLogger(CadastroDoacaoController.class);
 
     @PostMapping
-    public void cadastro(@RequestBody DadosPessoais dadosPessoais) {
+    public void cadastro(@RequestBody CadastroDoacao cadastroDoacao) {
 
-    logger.info(dadosPessoais.toString());
+        logger.info(cadastroDoacao.toString());
+    }
 
     }
-}
+
+
+
+
+
+
