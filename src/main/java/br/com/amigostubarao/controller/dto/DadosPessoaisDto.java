b2c21e36297d992f1609b9636acd8500fa054c2e
avocatsys <avocatsys@gmail.com>
2023-02-25 22:40:02 -0300

    test: Implementando testes.

---
+++ src/main/java/br/com/amigostubarao/controller/dto/DadosPessoaisDto.java
@@ -0,0 +1,36 @@
+package br.com.amigostubarao.controller.dto;
+
+import br.com.amigostubarao.model.DadosPessoais;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class DadosPessoaisDto {
+    public Long id;
+    public String nomeCompleto;
+    public String cpfCnpj;
+    public String email;
+    public String telefone;
+    public String dataNascimento;
+
+    public DadosPessoaisDto(Long id, String nomeCompleto, String cpfCnpj, String email, String telefone, String dataNascimento) {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.cpfCnpj = cpfCnpj;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public static DadosPessoaisDto from(DadosPessoais dadosPessoais) {
+        return new DadosPessoaisDto(
+                dadosPessoais.getId(),
+                dadosPessoais.getNomeCompleto(),
+                dadosPessoais.getCpfCnpj(),
+                dadosPessoais.getEmail(),
+                dadosPessoais.getTelefone(),
+                dadosPessoais.getDataNascimento()
+        );
+    }
+}
---
+++ src/test/java/br/com/amigostubarao/controller/DadosPessoaisControllerTests.java
@@ -0,0 +1,57 @@
+package br.com.amigostubarao.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DadosPessoaisControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("Cadastrar um novo registro no banco de dados do model Dados Pessoais.")
+    @Test
+    void dadoUmJsonDadosPessoaisQuandoCriarEntaoRetornaraHttpStatus201() throws Exception {
+
+        mockMvc.perform(post("/dados-pessoais")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(getJson()))
+                        .andDo(print())//Para ver logs.
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNotEmpty());
+    }
+
+    @DisplayName("Retonar todos os dados.")
+    @Test
+    void retonarTodosOsDadosPessoaisEhttpStatus200() throws Exception {
+
+        mockMvc.perform(get("/dados-pessoais")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    private String getJson() {
+        return "{  \n" +
+                "    \"nomeCompleto\":\"Mike\",\n" +
+                "    \"cpfCnpj\":\"22222222\",\n" +
+                "    \"email\":\"mike@gmail.com\",\n" +
+                "    \"telefone\":\"(11)92222-1111\",\n" +
+                "    \"dataNascimento\":\"01/01/1900\"\n" +
+                "}";
+    }
+}
